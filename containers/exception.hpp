7063214d4ee03933eb2c0cb9cf6841545cb1a32f
Leonid Shangin <leonid.shangin@math.msu.ru>
2022-12-17 10:18:29 +0300

    fix exceptions

--- containers/exception.hpp
+++ containers/exception.hpp
@@ -1,46 +1,53 @@
-#ifndef CUSTOM_EXCEPTION_HPP
-#define CUSTOM_EXCEPTION_HPP
+#ifndef CTL_EXCEPTION_HPP
+#define CTL_EXCEPTION_HPP
 
 
 #include <string>
 
 
-#define NEED_ABORT true
-#define NOT_NEED_ABORT false
+#define CTL_NEED_ABORT true
+#define CTL_NOT_NEED_ABORT false
 
 
-class custom_exception final
+namespace ctl { /* 'ctl' = custom template library */
+
+
+class exception final
 {
 
-	private:
+  private:
 
-		std::string _what;
-		std::string _where;
-		bool _need_abort;
+	std::string _what;  // The reason for the exception
+	std::string _where; // From where the exception was thrown
+	bool _need_abort;   // Marks if programm was crached and needs abort/terminate/...
 
-	public:
+  public:
 
-		custom_exception(void) = delete;
-		explicit custom_exception(const std::string & what, 
-								  const std::string & where = "", bool need_abort = true) :
+	exception(void) = delete;
+	explicit exception(const std::string & what,
+								const std::string & where = "", bool need_abort = CTL_NEED_ABORT) :
         _what(what), _where(where), _need_abort(need_abort)
-        { }
-		
-		~custom_exception() noexcept { }
+    { }
 
-		std::string what(void) const noexcept
-        { return _what; }
+	~exception() noexcept { }
 
-		std::string where(void) const noexcept
-        { return _where; }
+	std::string what(void) const noexcept
+    { return _what; }
 
-		bool need_abort(void) const noexcept
-        { return _need_abort; }
-		
-		std::string error_message(void) const noexcept
-        { return "'" + _what + "' in '" + _where + "'. " + (_need_abort ? "Crashed" : "No crash"); }
+	std::string where(void) const noexcept
+    { return _where; }
+
+	bool need_abort(void) const noexcept
+    { return _need_abort; }
+
+	std::string error_message(void) const noexcept
+    { return "'" + _what + "' in '" + _where + "'. " + (_need_abort ? "Crashed" : "No crash"); }
 
 };
 
 
-#endif /* CUSTOM_EXCEPTION_HPP */
+} /* namespace 'ctl' */
+
+
+#endif /* CTL_EXCEPTION_HPP */
+
